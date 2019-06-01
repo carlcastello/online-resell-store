0b109d93fd4b2a8ed0df74fd4f17c518ceeef74a
Carl Castello <carl.castello96@gmail.com>
2019-05-31 20:35:29 -0600

    ORS-1: Minor changes for the navbar

--- src/components/nav-bar/index.tsx
+++ src/components/nav-bar/index.tsx
@@ -2,25 +2,45 @@ import React, { Component } from "react";
 
 import { withStyles } from "@material-ui/styles";
 import { AppBar, Toolbar, Typography, IconButton } from "@material-ui/core";
-import { ShoppingCartOutlined } from '@material-ui/icons';
+import { ShoppingCartOutlined, Menu } from '@material-ui/icons';
 
 import styles from './styles';
 
 interface OwnProps {
   classes: {
+    mainToolBar: string,
+    secondaryToolBar: string,
+    menuButton: string
     title: string,
   }
 }
 
 class NavBar extends Component<OwnProps> {
+
+  renderMainToolBar = () => {
+    return (
+      <Toolbar className={this.props.classes.mainToolBar}>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+          <Menu/>
+        </IconButton>
+        <Typography variant="h6" color="inherit" className={this.props.classes.title}>
+          Online Resell Store
+        </Typography>
+        <div>
+          <IconButton >
+            <ShoppingCartOutlined/>
+          </IconButton>
+        </div>
+      </Toolbar>
+    ); 
+  }
+
   render() {
     return(
       <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="h6" color="inherit" className={this.props.classes.title}>
-            Online Resell Store
-          </Typography>
-          <div>
+        {this.renderMainToolBar()}
+        <Toolbar className={this.props.classes.secondaryToolBar}>
+        <div>
             <IconButton >
               <ShoppingCartOutlined/>
             </IconButton>
--- src/components/nav-bar/styles.tsx
+++ src/components/nav-bar/styles.tsx
@@ -1,5 +1,12 @@
-export default {
+const styles = (props, theme) => ({
+  mainToolBar: {
+    minHeight: '50px',
+  },
+  secondaryToolBar: {
+    minHeight: '0px',
+  },
   title: {
     flex: 1
   } 
-};
+})
+export default styles;
