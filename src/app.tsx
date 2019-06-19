30ab3c9cbe7135247c0523f3f0928a7a04d8c899
unknown <carl.castello96@gmail.com>
2019-06-18 21:21:18 -0600

    ORS-1: Move the navbar to the app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,12 @@ import AppTheme from './app-theme';
 import Home from './pages/home';
 
 import { ThemeProvider } from '@material-ui/styles';
+import NavBar from './components/nav-bar';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={AppTheme}>
+      <NavBar/>
       <Home/>
     </ThemeProvider>
   );
