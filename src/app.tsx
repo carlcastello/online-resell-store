84e7865ab4eb3131a1b854aad548a804015c8a9d
unknown <carl.castello96@gmail.com>
2019-07-07 13:29:53 -0600

    ORS-1: Integrated React Router"

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { ThemeProvider } from '@material-ui/styles';
 
 import NavBar from './components/nav-bar';
 import Home from './pages/home';
+import Product from './pages/product';
 import Footer from './components/footer';
 
 import AppTheme from './app-theme';
 
+
 const App: React.FC = () => {
   return (
-    <ThemeProvider theme={AppTheme}>
-      <NavBar/>
-      <Home/>
-      <Footer/>
-    </ThemeProvider>
+    <Router>
+      <ThemeProvider theme={AppTheme}>
+        <NavBar/>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/products" component={Home} />
+            <Route exact path="/product/:id" component={Product} />
+          </Switch>
+        <Footer/>
+      </ThemeProvider>
+    </Router>
   );
 }
 
---
+++ src/pages/product/index.tsx
@@ -0,0 +1,14 @@
+import React, { Component, ReactNode } from 'react';
+
+import { withStyles, Typography } from '@material-ui/core';
+
+class MainTitle extends Component {
+  render(): ReactNode {
+
+    return(
+      'hello'
+    );
+  }
+}
+
+export default MainTitle;
